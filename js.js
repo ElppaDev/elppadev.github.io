7616d57cd9d9a2246abe5940bd8b25fa69049214
ElppaDev <sa_8001@naver.com>
2018-05-13 23:07:19 +0900

    web

--- js.js
+++ js.js
@@ -12,7 +12,7 @@ function dday(y,m,d,id){
     var ddaytime = new Date(y,m-1,d);
     var dday2 = ddaytime.getTime() - nowtime.getTime();
     var day = Math.ceil(dday2 / (1000 * 60 * 60 * 24));
-    var hour = Math.ceil((dday2 % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var hour = Math.ceil((dday2 % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))-1;
     var min = Math.ceil((dday2 % (1000 * 60 * 60)) / (1000 * 60));
     var sec = Math.ceil((dday2 % (1000 * 60)) / 1000);
     $(id).html(day+"일 "+hour+"시 "+min+"분 "+sec+"초");
