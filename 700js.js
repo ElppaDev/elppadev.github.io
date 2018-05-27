1e57393eec2bda96d5fa7d0076574d51ca487437
ElppaDev <sa_8001@naver.com>
2018-05-28 00:01:32 +0900

    700day

--- 700js.js
+++ 700js.js
@@ -2,7 +2,7 @@ function dday(y,m,d,id){
     setInterval(function(){
     var nowtime = new Date(); 
     var ddaytime = new Date(y,m-1,d);
-    var dday2 = ddaytime.getTime() - nowtime.getTime();
+    var dday2 = nowtime.getTime() - ddaytime.getTime();
     var day = Math.ceil(dday2 / (1000 * 60 * 60 * 24));
     var hour = Math.ceil((dday2 % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))-1;
     var min = Math.ceil((dday2 % (1000 * 60 * 60)) / (1000 * 60));
@@ -13,5 +13,15 @@ function dday(y,m,d,id){
 }
 
 $(function(){
-    dday(2018,5,30,"#title")
+    dday(2016,6,29,"#title2")
 });
+
+function envetcard1(){
+    document.getElementById("msg").style.display="none";
+    document.getElementById("msg2").style.display="block";
+}
+
+function envetcard2(){
+    document.getElementById("msg").style.display="block";
+    document.getElementById("msg2").style.display="none";
+}
