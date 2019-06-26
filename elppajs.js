3b21e67eea44827ebecc6aabf8acfc87531bb282
sa_8001@naver.com <511817zxx>
2019-06-26 17:25:36 +0900

    .
    .

--- elppajs.js
+++ elppajs.js
@@ -2,7 +2,7 @@ function callbtn(){
     var fileter = "win16|win32|win64|mac|macintel";
     console.log("함수 수행");
     if(fileter.indexOf(navigator.platform.toLowerCase()) < 0){
-        location.open("tel://16440155");
+        alert("모바일 화면 입니다");
        }else{
            alert("문의 전화 : 1644-0155");
        }
