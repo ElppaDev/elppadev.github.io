0eeda45376ffbf8c0d2ab1fa9acafeb266bbae3f
sa_8001@naver.com <511817zxx>
2019-06-27 16:09:51 +0900

    .
    .

--- elppajs.js
+++ elppajs.js
@@ -4,6 +4,6 @@ function callbtn() {
     if (fileter.indexOf(navigator.platform.toLowerCase()) < 0) {
         location.replace("tel://16440155");
     } else {
-        alert("핸드폰 및 MacOS에서 클릭시 바로 전화로 연결됩니다!\n윈도우 이용자는 문의 전화 : 1644-0155로 연락주시면 감사하겠습니다!");
+        alert("핸드폰 및 MacOS에서 클릭시 바로 전화로 연결됩니다!\n윈도우 이용자는 전화 1644-0155로 연락주시면 감사하겠습니다!");
     }
 }
